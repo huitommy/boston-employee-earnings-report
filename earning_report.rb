a43b7c7b5eaada84ac0035b76e0cb803aee8538b
Tommy Hui <hui.1tommy@gmail.com>
2016-08-30 23:30:05 -0400

    generated show page with average salaries
    Included a list of job titles that were factored into average total earnings calculations. User can now search using /:title. Need to add function for if title does not exist.

--- earning_report.rb
+++ earning_report.rb
@@ -8,4 +8,12 @@ class EarningReport < Sinatra::Base
     @job_titles = earnings.map { |j| j.title }
     erb :index
   end
+
+  get "/:title" do
+    @job = params[:title].downcase
+    earning = Earning.all.select { |e| e.title.downcase.include?(@job)}
+    @job_titles = earning.map{ |j| j.title }.uniq
+    @average_earnings = earning.map { |e| e.total_earning.to_f }.reduce(:+) / earning.size
+    erb :show
+  end
 end

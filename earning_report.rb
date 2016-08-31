9fee1e00b8b45c8ddb3c94704c1e752865de3ed1
Tommy Hui <hui.1tommy@gmail.com>
2016-08-30 23:31:55 -0400

    added support for JSON requests
    Now handles both JSON and HTML (default) requests. Also fixed app for when queried job title does not exist. Need to add tests and add links to the index page.

--- earning_report.rb
+++ earning_report.rb
@@ -3,17 +3,40 @@ require "sinatra/base"
 require 'json'
 
 class EarningReport < Sinatra::Base
+
   get "/" do
     earnings = Earning.all.uniq { |e| e.title }
     @job_titles = earnings.map { |j| j.title }
-    erb :index
+    data = { job_titles: @job_titles }
+    request.accept.each do |type|
+      case type.to_s
+      when 'application/json'
+        halt data.to_json
+      else
+        halt erb :index
+      end
+    end
   end
 
   get "/:title" do
     @job = params[:title].downcase
     earning = Earning.all.select { |e| e.title.downcase.include?(@job)}
     @job_titles = earning.map{ |j| j.title }.uniq
-    @average_earnings = earning.map { |e| e.total_earning.to_f }.reduce(:+) / earning.size
-    erb :show
+    if earning.empty?
+      @average_earnings = 0
+      data = { "error": { code: 404, message: "Job title not found" } }
+    else
+      @average_earnings = earning.map { |e| e.total_earning.to_f }.reduce(:+) / earning.size
+      data = { "data": { title: @job, average_earnings: @average_earnings } }
+    end
+    request.accept.each do |type|
+      case type.to_s
+      when 'application/json'
+        halt data.to_json
+      else
+        halt erb :show
+      end
+    end
   end
+
 end
